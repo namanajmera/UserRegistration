815061d3cb587d07bded9ed7d6ce8296fd4c638c
Naman <cool.naman.ajmera@gmail.com>
2021-04-27 11:01:25 +0530

    Added: UserRegistration Validate First Name

---
+++ UserRegistration.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/**
+ * UserRegistration
+ */
+public class UserRegistration {
+
+    public static boolean validateFirstName(String name) {
+        String expression = "^[A-Z]{1}[a-zA-Z]{2,}";
+        if (name.matches(expression)) {
+            System.out.println("'" + name + "'" + " validation success !");
+            return true;
+        } else {
+            System.out.println("'" + name + "'" + " Invalid Input ");
+            return false;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration Project ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First name ");
+        String first_name = sc.nextLine();
+        System.out.println(validateFirstName(first_name));
+    }
+}
