7eeae2bd06f7276cd32ed26de49dd0fd52eb418b
Naman <cool.naman.ajmera@gmail.com>
2021-04-27 11:06:14 +0530

    Modified: Validating Last Name.

--- UserRegistration.java
+++ UserRegistration.java
@@ -17,11 +17,26 @@ public class UserRegistration {
 
     }
 
+    public static boolean validateLastName(String name) {
+        String expression = "^[A-Z]{1}[a-zA-Z]{2,}";
+        if (name.matches(expression)) {
+            System.out.println("'" + name + "'" + " validation success !");
+            return true;
+        } else {
+            System.out.println("'" + name + "'" + " Invalid Input ");
+            return false;
+        }
+
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Project ");
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter First name ");
         String first_name = sc.nextLine();
         System.out.println(validateFirstName(first_name));
+        System.out.println("Enter Last name ");
+        String last_name = sc.nextLine();
+        System.out.println(validateLastName(last_name));
     }
 }
